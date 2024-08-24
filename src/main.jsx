8ceaec2ea35d0e74bf02019555086eaaa456e156
Lansarbac2020 <secretariat.lansarcenter@gmail.com>
2024-08-24 18:56:37 +0300

    clerk

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './home'
+import Contact from './contact'
+const router =createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/contact',
+    element:<Contact/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
