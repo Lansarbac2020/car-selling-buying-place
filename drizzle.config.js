7b9ab465881fe89ee2f49603a17575f464526950
Lansarbac2020 <secretariat.lansarcenter@gmail.com>
2024-09-13 11:29:18 +0300

    quick fix

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./config/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://carmarketplace_owner:2jhTWJeDEI6M@ep-young-math-a54vh5t7.us-east-2.aws.neon.tech/carmarketplace?sslmode=require',
+      url: import.meta.VITE_POSGRESQL_URL,
     }
   };
